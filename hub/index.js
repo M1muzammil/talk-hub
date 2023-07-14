182bcd308c95380400adc71dbfe4a8ba99ddee0d
Muhammad Muzammil Ali <119755415+M1muzammil@users.noreply.github.com>
2023-07-14 06:13:44 -0700

    comment not ready

--- hub/index.js
+++ hub/index.js
@@ -1,47 +1,218 @@
+// import {  getFirestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// import { firestore } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 
-//   const firebaseConfig = {
-//     apiKey: "AIzaSyA6OfqxFMpxuQ4fsDid-oeWxdUhelwoMxI",
-//     authDomain: "talk-hub-f1982.firebaseapp.com",
-//    projectId: "talk-hub-f1982",
-//     storageBucket: "talk-hub-f1982.appspot.com",
-//     messagingSenderId: "325725732271",
-//    appId: "1:325725732271:web:04b719d20dde34d1f7035a"
-//  };
-  
-  
-//  const app = initializeApp(firebaseConfig);
-//  const db = firebase.firestore();
-//  function creatmsg() {
 
-//  const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-  
-//  // Get input values
-//  const message= document.getElementById("message").value;
+  const firebaseConfig = {
+    apiKey: "AIzaSyA6OfqxFMpxuQ4fsDid-oeWxdUhelwoMxI",
+    authDomain: "talk-hub-f1982.firebaseapp.com",
+   projectId: "talk-hub-f1982",
+    storageBucket: "talk-hub-f1982.appspot.com",
+    messagingSenderId: "325725732271",
+   appId: "1:325725732271:web:04b719d20dde34d1f7035a"
+ };
 
-
-//  db.collection("talkhub").add({
-//    timestamp: timestamp,
-//    message: message,
-   
-//  })
-//    .then((docRef) => {
-//      console.log("Document written with ID: ", docRef.id);
-//      rendermessage();
-//    })
-//    .catch((error) => {
-//      console.error("Error adding document: ", error);
-//    });
-
-//  // Clear input fields
-//  document.getElementById("message").value = "";
+ firebase.initializeApp(firebaseConfig);
+ const db = firebase.firestore();
  
-// }
+ 
+ function createPoll(event) {
+   event.preventDefault();
+ 
+   // Get the current timestamp
+   const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+ 
+   // Get input values
+   const name = prompt("your name")
+   
+   const message = document.getElementById("message").value;
+//    const commentmsg = commentmsg
+   db.collection("talkhub")
+     .add({
+       timestamp: timestamp,
+       message: message,
+       name : name
+    //    commentmsg: commentmsg
+     })
+     .then((docRef) => {
+       console.log("Document written with ID:", docRef.id);
+       rendermsg();
+     })
+     .catch((error) => {
+       console.error("Error adding document:", error);
+     });
+ 
+   // Clear input fields
+   document.getElementById("message").value = "";
+ }
+ 
+ function rendermsg() {
+    const container = document.querySelector(".container");
+  
+    db.collection("talkhub")
+      .orderBy("timestamp", "desc")
+      .get()
+      .then((querySnapshot) => {
+        container.innerHTML = ""; // Clear previous chat list
+  
+        if (querySnapshot.empty) {
+          container.innerText = "No chat found";
+        } else {
+          const reversedDocs = querySnapshot.docs.reverse(); // Reverse the order of the documents
+  
+          reversedDocs.forEach((doc) => {
+            const data = doc.data();
+  
+            const maindiv = document.createElement("div");
+            maindiv.className = "maindiv";
+  
+            const forimage = document.createElement("div");
+            forimage.className = "forimage";
+            maindiv.appendChild(forimage);
 
 
-let input = document.querySelector("form input")
-let container = document.querySelector(".container")
-let form = document.querySelector("form")
+
+          
+
+
+
+            const image = document.createElement("img");
+            image.src = "./profile.png";
+            image.style.width = "50px";
+            image.style.height = "50px";
+            forimage.appendChild(image);
+  
+
+            const text = document.createElement("span"); // Create a text element
+            text.innerText = data.name ; // Set the text content
+            forimage.appendChild(text);
+
+
+
+            const heading = document.createElement("p");
+            heading.className = "heading";
+            heading.innerText = data.message;
+            maindiv.appendChild(heading);
+  
+            const foter = document.createElement("div");
+            foter.className = "foter";
+            maindiv.appendChild(foter);
+  
+            const comment = document.createElement("button");
+            comment.className = "btn btn-primary btn-sm button";
+            comment.innerHTML = "<i class='bi bi-chat-left-text-fill'></i>";
+            foter.appendChild(comment);
+  
+            const del1 = document.createElement("button");
+            del1.className = "btn btn-danger btn-sm button";
+            del1.innerHTML = "<i class='bi bi-trash-fill'></i>";
+            del1.addEventListener("click", () => deletePoll(doc.id));
+            foter.appendChild(del1);
+  
+            const share = document.createElement("button");
+            share.className = "btn btn-dark btn-sm button";
+            share.innerHTML = "<i class='bi bi-share-fill'></i>";
+            foter.appendChild(share);
+  
+            container.insertBefore(maindiv, container.firstChild);
+  
+            comment.addEventListener("click", () => {
+              const commentdiv = document.createElement("div");
+              commentdiv.className = "comentdiv";
+              maindiv.appendChild(commentdiv);
+  
+              const row1 = document.createElement("div");
+              row1.className = "row1";
+              commentdiv.appendChild(row1);
+  
+              const row2 = document.createElement("div");
+              row2.className = "row2";
+              commentdiv.appendChild(row2);
+  
+              const commentmsg = prompt("Enter your comment:");
+              const commentbox = document.createElement("div");
+              commentbox.className = "comentbox";
+              commentbox.innerText = commentmsg;
+              row1.appendChild(commentbox);
+  
+              const del = document.createElement("button");
+              del.className = "btn btn-danger btn-sm button";
+              del.innerHTML = "<i class='bi bi-trash-fill'></i>";
+              row2.appendChild(del);
+            });
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching chat:", error);
+      });
+  }
+ 
+ function deletePoll(docId) {
+   const password = prompt("Enter password to delete the poll:");
+ 
+   if (password === "muzammil") {
+     return db
+       .collection("talkhub")
+       .doc(docId)
+       .delete()
+       .then(() => {
+         rendermsg();
+       })
+       .catch((error) => {
+         console.error("Error deleting poll:", error);
+       });
+   } else {
+     alert("Incorrect password. Poll not deleted.");
+   }
+ }
+ 
+ document.addEventListener("DOMContentLoaded", function () {
+   rendermsg();
+ });
+ 
+ const form = document.querySelector("form");
+ const input = document.querySelector("form input");
+ 
+ form.addEventListener("submit", createPoll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input = document.querySelector("form input")
+// let container = document.querySelector(".container")
+// let form = document.querySelector("form")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -53,120 +224,6 @@ form.addEventListener("submit", (event) => {
 
 
 
-
-let maindiv = document.createElement("div");
-maindiv.className +="maindiv"
-container.appendChild(maindiv);
-
-
-
-
-let forimage = document.createElement("div");
-forimage.className +="forimage";
-maindiv.appendChild(forimage);
-
-let image = document.createElement("img");
- 
-image.src = "./profile.png";
-image.style.width = "50px";
-image.style.height = "50px";
-  forimage.appendChild(image);
-
-
-
-
-let heading = document.createElement("p")
-    heading.className += " heading"
-    heading.innerText = input.value
-    maindiv.appendChild(heading)
-
-
-
-
-
-
-    let foter = document.createElement("div");
-    foter.className += "foter";
-    maindiv.appendChild(foter)
-
-
-
-
-     let comment = document.createElement("button")
-      comment.className += "btn btn-primary btn-sm button"
-     comment.innerHTML = "<i class='bi bi-chat-left-text-fill'></i>"
-     foter.appendChild(comment)
- let del = document.createElement("button")
-     del.className += "btn btn-danger btn-sm button"
-      del.innerHTML = "<i class='bi bi-trash-fill'></i>"
-     foter.appendChild(del)
-
-
- let share = document.createElement("button")
-       share.className += "btn btn-dark btn-sm button"
-       share.innerHTML = "<i class='bi bi-share-fill'></i>"
-     foter.appendChild(share)
-
-
-
-
-
-
-
-
-
-
-     container.insertBefore(maindiv, container.firstChild);
-
-
-
-comment.addEventListener("click" , ()=>{
-
-    
-
-    let commentdiv = document.createElement("div");
-    commentdiv.className+= "comentdiv"
-    maindiv.appendChild(commentdiv)  
-
-    
-
-
-
-
-let row1 = document.createElement("row");
-row1.className+= "row1";
-commentdiv.appendChild(row1)
-let row2 = document.createElement("row");
-row1.className+= "row2";
-commentdiv.appendChild(row2)
-
-
-let commentmsg = prompt("enter your comment ");
-let comentbox = document.createElement("div");
-comentbox.className += "comentbox";
-comentbox.innerText = commentmsg
-row1.appendChild(comentbox);
-
-
-let del = document.createElement("button")
-del.className += "btn btn-danger btn-sm button"
- del.innerHTML = "<i class='bi bi-trash-fill'></i>"
-row2.appendChild(del)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
 
 
 
